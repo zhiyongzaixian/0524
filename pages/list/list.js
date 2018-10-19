e8c1d0a8fa5e3aa0077f60a11a40f71db0b31c6d
zhiyongzaixian <13852292775@163.com>
2018-10-19 16:20:18 +0800

    'init'

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,18 +1,21 @@
 // pages/list/list.js
+//  注意只能使用相对路径
+let listArr = require('../../datas/list-data.js');
+console.log(listArr, typeof listArr);
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+		listArr: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+		this.setData({listArr: listArr.list_data});
   },
 
   /**
